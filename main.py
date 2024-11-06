571886106d61b94f4e12c21ae7290a48dca7f412
stumpfi <stefan.stumpf92@googlemail.com>
2024-11-06 15:30:42 +0100

    112412414214

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ req = Request(
 page = urlopen(req)
 html = page.read().decode("utf-8")
 
-print(html)
+a = open('result.html', 'w')
+a.write(html)
+a.close()
 
 a = open('result.txt', 'w')
 a.write(html)
 a.close()
 
-with open('result.pdf', "wb") as pdf_file:
-    pisa_status = pisa.CreatePDF(html, dest=pdf_file)        
+# with open('result.pdf', "wb") as pdf_file:
+#     pisa_status = pisa.CreatePDF(html, dest=pdf_file)        
 
 
 
