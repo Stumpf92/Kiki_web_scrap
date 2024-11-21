d98c6155e431ec5ca401e279ecd59ab504802251
Stefan Stumpf <stefan.stumpf92@googlemail.com>
2024-11-21 19:24:25 +0100

    123123123

--- user_control.py
+++ user_control.py
@@ -1,35 +1,33 @@
 from database import Database
-from article import Article
-from article import Articles
+
 
 db = Database("localhost","postgres","postgres","dfgtzu88",5432)
 db.connect()
-db.synch()
 
-##### CREATE A TABLE
-db.create_table('testtest',['source TEXT','url TEXT','author TEXT','release_date TEXT','title TEXT','text TEXT','tags TEXT','import_time TEXT'])
-
-##### LOAD PARTS OF URL_LIST
-list_of_links = []
-with open("billboard/links.txt", "r") as file:
-    for line in file:        
-        list_of_links.append(line)
-counter = 40000
-step_size = 40000
-
-for url in list_of_links[counter:(counter+step_size)]:    
-    print(counter)
-    db.add(url)
-
-db.set_primary_id('testtest')
-
-##### SYNACH db WITH PSQL
-db.synch('testtest')
-db.overview()
+table = 'all_articles' # ACHTUNG in def db_add auch ändern ....
+columns = [['article_id','INT'],
+           ['source','TEXT'],
+           ['url','TEXT'],
+           ['author','TEXT'],
+           ['release_date','TEXT'],
+           ['title','TEXT'],
+           ['text','TEXT'],
+           ['tags','TEXT'],
+           ['import_time','TEXT'],
+            ['matches','TEXT']]
+import_link = "billboard/links.txt"
+source = "BILLBOARD"
+#filter_list = ['Kevin','statement','lil']
+#filter_list = ['nda','ndas','disclosure','confidentiality','confidential','disclosed','undisclosed','secret','contract']
+filter_list = ['confidentiality','confidential']
+filter = '(?:^|(?<= ))('+'|'.join(filter_list) + ')(?:(?= )|$)'
 
 
-# #### FILTER WHOLE db WITH FILTER AND PRINT THEM TO PDF
-# articles = Articles(db)
-# articles.print_pdf(articles.filter(['trial','peter']))
+#### SYNCH db WITH PSQL
+db.synch(table, columns)
+#db.overview()
+
+db.filter(filter, filter_list, save_state = False, print_state = True)
+
 
 db.disconnect()
