32a091134adee730f9008d920ef066d5022ac665
Stefan Stumpf <stefan.stumpf92@googlemail.com>
2025-03-24 09:40:19 +0100

    printing_version

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 from database import Database
 import time
-# from xhtml2pdf import pisa
 from weasyprint import HTML
+import re
 
 db = Database("localhost","postgres","postgres","dfgtzu88",5432)
 db.connect()
@@ -24,155 +24,88 @@ dataframe = db.synch(table, columns)
 
 # declare a pattern
 # words = ['nda','ndas','disclosure','confidentiality','confidential','disclosed','undisclosed','secret','contract']
-words = ['hitler']
+words = ['nda','ndas']
 patter = r'(?i)(?:^|(?<= ))('+'|'.join(words) + ')(?:(?= )|$)'
 
 mask = dataframe[['text','title']].apply(lambda x: x.str.contains(patter, regex=True).any(), axis=1)
 
 filtered = dataframe[mask]
 
-test = filtered[:5]
+def markiere_woerter(html_text, woerter_liste):
+    for wort in woerter_liste:
+        # Regex für exakte Wortübereinstimmung (case-insensitive)
+        html_text = re.sub(rf'\b({re.escape(wort)})\b', r'<mark>\1</mark>', html_text, flags=re.IGNORECASE)
+    return html_text
 
-
-# # create html
-# all_html = []
-
-# # 1st page
-# all_html.append("<html lang=\"de\"><head><style>@page { size: A4; margin: 2cm; @frame footer_frame { -pdf-frame-content: footer_content; bottom: 10px; height: 50px; } }.seitenumbruch { page-break-before: always; }</style></head>")
-# all_html.append("<html><body>")
-# all_html.append("<p>")
-# all_html.append("REPORT")
-# all_html.append("</p>")
-# heutiges_datum = time.strftime("%d.%m.%Y")
-# all_html.append("<p>")
-# all_html.append("date: " + str(heutiges_datum))
-# all_html.append("<br>")
-# all_html.append("number of evaluated articles: " + str(dataframe.shape[0]))
-# all_html.append("<br>")
-# all_html.append("number of interesting articles: " + str(len(filtered)))
-# all_html.append("<br>")
-# all_html.append("used filter: " + str(words))
-# all_html.append("</p>")
-# all_html.append("<div id=\"footer_content\"> Seite <pdf:page> von <pdf:pageCount> </div>")
-# all_html.append("<div class=\"seitenumbruch\"></div>")
-
-# # next pages
-# all_html.append("<p>")
-# all_html.append("REPORT")
-# all_html.append("</p>")
-# heutiges_datum = time.strftime("%d.%m.%Y")
-# all_html.append("<p>")
-# all_html.append("date: " + str(heutiges_datum))
-# all_html.append("<br>")
-# all_html.append("number of evaluated articles: " + str(dataframe.shape[0]))
-# all_html.append("<br>")
-# all_html.append("number of interesting articles: " + str(len(filtered)))
-# all_html.append("<br>")
-# all_html.append("used filter: " + str(words))
-# all_html.append("</p>")
-
-
-
-# overview = []
-# overview.append("<html><body>")
-# for row in filtered:
-#     overview.append("<p>")
-#     overview.append("<a href=")
-#     overview.append(row["url"])
-#     overview.append(">")
-#     overview.append(row.url)
-#     overview.append("</a>")
-#     overview.append("</p>")
-#     overview.append("<p>")
-#     overview.append('Release_date:   ')
-#     overview.append(str(row.release_date))
-#     overview.append("</p>")
-#     overview.append("<p>")
-#     overview.append('Artikel_id:   ')
-#     overview.append(str(row.article_id))
-#     overview.append("</p>")
-#     overview.append("<p>")
-#     overview.append('Filter:   ')
-#     overview.append(str(filter_list))
-#     overview.append("</p>")
-#     overview.append("<p>")
-#     overview.append('Number of Matches:   ')
-#     overview.append(str(row.number_of_matches))
-#     overview.append("</p>")
-#     overview.append("<p><u>")
-#     overview.append(str(row.title))
-#     overview.append("</u></p>")
-#     overview.append("<p>")
-#     overview.append(str(row.text))
-#     overview.append("</p>")
-#     overview.append("<p>")
-#     overview.append('TAGS:   ')
-#     overview.append(str(row.tags))
-#     overview.append("</p>")
-#     overview.append("<br> </br>")
-#     overview.append("<br> </br>")
-#     overview.append("<br> </br>")
-# overview.append("</body></html>")
-
-
-
-# html = "".join(overview)
-# for word in filter_list:
-#     fil = "\\b"+word+"\\b"
-#     sub = "<b>"+word+"</b>"
-#     print (fil, "     ", sub)
-#     html = re.sub(fil, sub, html, flags= re.I)
-
-# complete_html = "".join(all_html)
-# with open('test.txt', 'w',  encoding="utf-8") as a:
-#     a.write(complete_html)
-#     a.close()
-
-
-# with open ("test.pdf", "w+b") as result_file:
-#     pisa.CreatePDF(complete_html, dest= result_file)
-
-# HTML mit Styles für Seitenzahlen und Seitenumbrüche
-first_html = """
+heutiges_datum = time.strftime("%d.%m.%Y, %H:%M:%S")
+first_html = f"""
 <html>
 <head>
     <style>
-        @page {
+        @page {{
             size: A4;
             margin: 2cm;
 
-            /* Seitenzahlen in der Mitte der Fußzeile */
-            @bottom-center {
+            @bottom-center {{
                 content: "Seite " counter(page) " von " counter(pages);
-            }
-        }
+                font-family: Arial;
+                font-size: 13pt;
+                text-align: center;
+            }}
+        }}
 
-        /* Zentrierter Text auf der Seite */
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
+        .page-content{{
+            text-align: left;
+            padding: 0px;
+        }}
 
-        .page-content {
-            text-align: center;
-            padding: 50px;
-        }
-
-        .seitenumbruch {
-            page-break-before: always; /* Erzwingt einen Seitenumbruch */
-        }
+        .seitenumbruch {{
+            page-break-before: always;
+        }}
     </style>
 </head>
 <body>
+    <div class="page-content">
+        <p style="font-size: 20px; font-family: Arial; font-weight: bold; text-align: center">
+        Report
+        </p>
+        <p style="font-size: 13px; font-family: Arial;">
+        date: {heutiges_datum}
+        </br>
+        number of evaluated articles: {str(dataframe.shape[0])}
+        </br>
+        number of interesting articles: {str(len(filtered))}
+        </br>
+        filter by: {str(words)}
+        </br>
+        sorted by: highest number of matches first
+        </p>
+    </div>
+    <div class="seitenumbruch"></div>
 """
+
 
 # HTML für mehrere Seiten generieren (dies ist nur ein Beispiel, wie du mehrere Seiten erstellen könntest)
 next_html = ""
-for i in range(1, 6):  # Erstelle Seiten 1 bis 5
+for index, row in filtered.iterrows():  # Erstelle Seiten 1 bis 5
+    url = str(row['url'])
+    date = str(row['release_date'])
+    titel = markiere_woerter(str(row['title']), words)
+    text = markiere_woerter(str(row['text']), words)
     page_html = f"""
     <div class="page-content">
-        <h1>PDF mit Seitenzahlen</h1>
-        <p>Dies ist Seite {i}</p>
+        <p style="font-size: 13px; font-family: Arial;">
+        <a href="{url}">{url}</a>
+        </p>
+        <p style="font-size: 13px; font-family: Arial;">
+        {date}
+        </p>
+        <p style="font-size: 15px; font-family: Arial; text-decoration: underline">
+        {titel}
+        </p>
+        <p style="font-size: 13px; font-family: Arial;">
+        {text}
+        </p>
     </div>
     <div class="seitenumbruch"></div>
     """
@@ -188,7 +121,8 @@ last_html = """
 complete_html = first_html + next_html + last_html
 
 # Das HTML in eine PDF-Datei umwandeln
-HTML(string=complete_html).write_pdf("output.pdf")
+jetzt = str(round(time.time()))
+HTML(string=complete_html).write_pdf(f"search_reports/{jetzt}_output.pdf")
 
 print("PDF erfolgreich erstellt!")
 
